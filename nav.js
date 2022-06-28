d004fa6844953e5159199a81b3331f5384362900
Shaurya Sehgal <56615472+0shaurya@users.noreply.github.com>
2022-06-28 17:48:44 -0500

    Update nav.js

--- nav.js
+++ nav.js
@@ -3,7 +3,7 @@ window.onload=function() {
 const sidebarHomeB = document.getElementById("sidebarHomeB")
 
 sidebarHomeB.addEventListener("click", function() {
-	window.location.href = "/";
+	window.location.href = "/enginuity";
 })
 
 document.getElementById("navAlgebra").style.display = "none";
